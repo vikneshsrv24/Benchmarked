c83a20cd33abe7179ab6747d267d1c3c22d3f78b
SRV <97838319+vikneshsrv24@users.noreply.github.com>
2026-01-23 22:55:33 +0530

    streamlit update static 1o1

--- benchmarked.py
+++ benchmarked.py
@@ -1,5 +1,16 @@
 import yfinance as yf
+import streamlit as st
+
+# Adding title to the website.
+st.title("Project: Benchmaked")
+st.subheader("Does Nifty50 actually helps us?")
 
 df = yf.download('^NSEI', start='2020-01-01', end='2025-01-01')
 
-print(df.head())
+# Display data in a table
+st.write("### Raw Market Data")
+st.dataframe(df.head())
+
+# Display a line chart
+st.write('Nifty 50 closing price trend')
+st.line_chart(df['Close'])
